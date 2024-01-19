ab25de5555261a6ef9cb74932267e8e5c924e423
longduongups <longduong1608@gmail.com>
2024-01-19 09:03:42 +0100

    TP1 test lesgaulois

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,5 +1,23 @@
 package personnages;
 
 public class Gaulois {
-
+    private String nom;
+    private int force;
+    public Gaulois(String nom, int force) {
+    	this.nom=nom;
+    	this.force=force;
+    }
+    public String getNom() {
+    	return this.nom;
+    }
+    void parler(String text) {
+    	System.out.print("Le gaulois " + this.nom +": " +text);
+    }
+    public static void main(String[] args) {
+    	Gaulois asterix =new Gaulois("Asterix",8);
+		System.out.println(asterix.getNom());
+		asterix.parler("Bonjour a tous");
+    }
+   
 }
+	
